d819de5b509d86c4dc76ed5962839178e660b360
tskhvedian <t.tskhvediani@gmail.com>
2024-03-05 13:01:32 +0400

    forms-starter

---
+++ src/components/YouTubeForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const YouTubeForm = () => {
+  return (
+    <div>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" name="channel" />
+
+        <button>Send</button>
+      </form>
+    </div>
+  );
+};
